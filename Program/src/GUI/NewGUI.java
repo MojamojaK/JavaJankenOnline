7b21fb083ea913079f78af20fbecf70f3d7efec6
rastaman7 <34154020+rastaman7@users.noreply.github.com>
2018-05-09 22:48:17 +0900

    Update NewGUI.java

--- Program/src/GUI/NewGUI.java
+++ Program/src/GUI/NewGUI.java
@@ -18,14 +18,14 @@ public class NewGUI extends JFrame implements ActionListener {
     public static void main(String[] args) {
         NewGUI frame = new NewGUI("ジャンケン");
         frame.setVisible(true);
-
+        
     }
-
+    
     JButton button1;
     JButton button2;
     JButton button3;
     JButton button4;
-
+    
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == button1) {
             System.out.println("g");
@@ -33,29 +33,34 @@ public class NewGUI extends JFrame implements ActionListener {
             //dispose();
             //System.exit(0);
         } else if (e.getSource() == button2) {
-             System.out.println("c");
+            System.out.println("c");
             addChar('c');
-             //dispose();
-             //System.exit(0);
+            //dispose();
+            //System.exit(0);
         } else if (e.getSource() == button3) {
             System.out.println("p");
             addChar('p');
             //dispose();
-            //System.exit(0); 
+            //System.exit(0);
+        }else if (e.getSource() == button4) {
+            System.out.println("S");
+            addChar('S');
+            //dispose();
+            //System.exit(0);
         }
     }
-
+    
     public NewGUI(String title) {     //String title
         {
             setTitle(title);
-
+            
             setBounds(100, 100, 900, 300);
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             JLabel labelSubTitle = new JLabel("どれかボタンを押してね");
             super.add("North", labelSubTitle);
-
+            
             JPanel p = new JPanel();
-
+            
             ImageIcon icon1 = new ImageIcon("images/rock.jpg");
             button1 = new JButton("rock", icon1);
             ImageIcon icon2 = new ImageIcon("images/scissors.jpg");
@@ -63,22 +68,22 @@ public class NewGUI extends JFrame implements ActionListener {
             ImageIcon icon3 = new ImageIcon("images/paper.jpg");
             button3 = new JButton("paper", icon3);
             button4 = new JButton("Start");
-
+            
             button1.addActionListener(this);
             button2.addActionListener(this);
             button3.addActionListener(this);
             button4.addActionListener(this);
-
+            
             p.add(button1);
             p.add(button2);
             p.add(button3);
             p.add(button4);
-
-
+            
+            
             Container contentPane = getContentPane();
             contentPane.add(p, BorderLayout.CENTER);
         }
-
+        
     }
     
     public synchronized int inboxSize() {
